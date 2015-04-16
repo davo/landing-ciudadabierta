1f1f5533316b76bbb854ef508c82e3abeb949bb4
Davo <davidgalavotti@gmail.com>
2015-04-16 12:19:30 -0300

    Agrego particulas e imagen nueva

--- js/main.js
+++ js/main.js
@@ -8,12 +8,71 @@ $(".main").onepage_scroll({
 	updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
 	beforeMove: function(index) { // This option accepts a callback function. The function will be called before the page moves.
 
-	},  
+	},
 	afterMove: function(index) { // This option accepts a callback function. The function will be called after the page moves.
 
-	},   
+	},
 	loop: false,
 	keyboard: true,
 	responsiveFallback: false,
 	direction: "vertical"
+});
+
+function randomNumber(minimum, maximum){
+    return Math.round( Math.random() * (maximum - minimum) + minimum);
+}
+
+// Inicializo el plugin particlesJS
+
+particlesJS('constelacion', {
+  particles: {
+    color: '#fff',
+    color_random: false,
+    shape: 'circle',
+    opacity: {
+      opacity: 1,
+      anim: {
+        enable: true,
+        speed: 0.5,
+        opacity_min: 0,
+        sync: true
+      }
+    },
+    size: randomNumber(3,7),
+    size_random: true,
+    nb: randomNumber(30,45),
+    line_linked: {
+      enable_auto: true,
+      distance: randomNumber(200,300),
+      color: '#fff',
+      opacity: .9,
+      width: .5,
+      condensed_mode: {
+        enable: false,
+        rotateX: 900,
+        rotateY: 900
+      }
+    },
+    anim: {
+      enable: true,
+      speed: 1.8
+    }
+  },
+  interactivity: {
+    enable: false,
+    detect_on: 'canvas',
+    mode: 'grab',
+    line_linked: {
+      opacity: .5
+    },
+    events: {
+      onresize: {
+        enable: true,
+        mode: 'out',
+        density_auto: true,
+        density_area: randomNumber(1000,1500)
+      }
+    }
+  },
+  retina_detect: true
 });
