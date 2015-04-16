aa7042920f7477b331fb6fc4bca246af70590214
Nicols <nlound@gmail.com>
2015-04-16 12:25:12 -0300

    agrego botones de next

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // Inicializo el plugin OnePageScroll
-
 $(".main").onepage_scroll({
 	sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
 	easing: "ease",                  // "ease", "linear", "ease-in", "ease-out", "ease-in-out", "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
@@ -7,13 +6,17 @@ $(".main").onepage_scroll({
 	pagination: false,                // You can either show or hide the pagination. Toggle true for show, false for hide.
 	updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
 	beforeMove: function(index) { // This option accepts a callback function. The function will be called before the page moves.
-
 	},  
 	afterMove: function(index) { // This option accepts a callback function. The function will be called after the page moves.
-
 	},   
 	loop: false,
 	keyboard: true,
 	responsiveFallback: false,
 	direction: "vertical"
+});
+
+
+// escucho todos los avanzar para 
+$(".avanzar").click(function() {
+    $(".main").moveDown();
 });
